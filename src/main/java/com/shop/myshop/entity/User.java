948d439d5578b104636beec8d7c55a81c43a8481
jianhuilv <690607189@qq.com>
2021-11-30 18:02:53 +0800

    First Commit

--- src/main/java/com/shop/myshop/entity/User.java
+++ src/main/java/com/shop/myshop/entity/User.java
@@ -2,10 +2,12 @@ package com.shop.myshop.entity;
 
 public class User {
 
+
     int uid;
     String password;
     String mail;
     String username;
+    boolean admin;
     @Override
     public String toString() {
         return "User{" +
@@ -14,6 +16,14 @@ public class User {
                 ", mail='" + mail + '\'' +
                 ", username='" + username + '\'' +
                 '}';
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
 
     public int getUid() {
